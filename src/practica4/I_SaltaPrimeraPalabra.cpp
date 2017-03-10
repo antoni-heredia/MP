87d4e74e2f81f4c2bf5ea911a3541304af2e97f9
antoni-heredia <a.heredia.castillo@gmail.com>
2017-03-10 18:23:00 +0100

    Ejercicio 8 Practica 4
    Añadido el ejercicio 8 de la practica cuatro. En el ejercicio 7 solo he eliminado una linea de *

--- src/practica4/I_SaltaPrimeraPalabra.cpp
+++ src/practica4/I_SaltaPrimeraPalabra.cpp
@@ -9,7 +9,7 @@
 // PRÁCTICA 4
 //
 /*
-  Lee una cadena y localiza la posición del primer carácter espacio
+  Lee una cadena y la imprime saltandose la primera palabra
 */
 /*********************************************************************/
 
@@ -42,9 +42,13 @@ int main()
   cout << "Introduce una cadena: ";
   cin.getline(cadena,LONGITUD);
 
-  //Mostramos la posicion donde se encuentra el espacio en la cadena.
-  cout << "La posicion del primer espacio en la cadena es: " <<
-  PosicionPrimerEspacio(cadena, strlen(cadena)) << endl;
+  //Guardamos la posicion donde esta el primer espacio
+  int posicion = PosicionPrimerEspacio(cadena, strlen(cadena));
+
+  //La sobrecarga del operador << nos permite imprimir la cadena a partir de la
+  //posicion despues del espacio hasta el final de la cadena clasica
+  cout << "La frase a partir del primer espacio es: " << &cadena[posicion+1]
+   << endl;
 
 	return (0);
 }
